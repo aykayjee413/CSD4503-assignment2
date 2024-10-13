9e7c58dc0847fe0b5ff425a266bc956ccc189cbd
c0870757 <c0870757@mylambton.ca>
2024-10-12 23:00:42 -0400

    wahoo

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ mongodb_client = MongoClient("mongodb+srv://Lauren:qyDCpfewVq12UULK@cluster0.wvi
 db = mongodb_client["shop_db"]
 products_collection = db["products"]
 mock_datat = [
-    {"name": "child", "tag":"a real child. his name is Gabriel.", "price": 1000.00, "image_path": "/static/images/child.jpg"},
-    {"name": "Balatro", "tag": "Balatro Steam key.", "price": 15.00, "image_path": "/static/images/balatro.jpg"},
-    {"name": "Stardew Valley", "tag": "Stardew Valley Steam key.", "price": 10.00, "image_path": "/static/images/stardew_valley.jpg"},
-    {"name": "Elden Ring", "tag": "Elden Ring Steam key.", "price": 50.00, "image_path": "/static/images/elden_ring.jpg"}
+    {"name": "child", "tag":"a real child. his name is Gabriel.", "price": 999.99, "image_path": "/static/images/child.jpg"},
+    {"name": "Balatro", "tag": "Balatro Steam key.", "price": 15.50, "image_path": "/static/images/balatro.jpg"},
+    {"name": "Stardew Valley", "tag": "Stardew Valley Steam key.", "price": 10.50, "image_path": "/static/images/stardew_valley.jpg"},
+    {"name": "Elden Ring", "tag": "Elden Ring Steam key.", "price": 50.50, "image_path": "/static/images/elden_ring.jpg"}
              ]
-products_collection.insert_many(mock_datat)
+# products_collection.insert_many(mock_datat)
 
 @app.route("/")
 def index():
